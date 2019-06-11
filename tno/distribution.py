Factor of 2 wrong

tno/distribution.py
@@ -118,7 +118,7 @@ class DoublePowerLaw(AnalyticDistribution):
 class BrownDistribution(AnalyticDistribution):
 	def __init__(self, x_min, x_max, sigma):
 		self.sigma = sigma
-		self.f = lambda x : np.sin(x * np.pi/180) * np.exp(- (x/sigma)**2)
+		self.f = lambda x : np.sin(x * np.pi/180) * np.exp(- (x/sigma)**2/2.)
 		AnalyticDistribution.__init__(self, x_min, x_max, self.f)
 
 '''