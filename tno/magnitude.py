006bb230a41579c70cd4767cf665226f441b7e4f
Pedro Bernardinelli <pedro.h.bernardinelli@gmail.com>
2020-03-04 10:41:24 -0500

    simpler approach to colors

--- tno/magnitude.py
+++ tno/magnitude.py
@@ -1,72 +1,24 @@
 '''
 Series of convenience functions for working with magnitudes
 '''
-import os 
 import numpy as np 
 import astropy.table as tb 
 
-from scipy.integrate import simps
+from scipy.integrate import trapz
 
-orbdata = os.getenv('DESDATA')
+alpha_i  =  1.492983987794142 
+beta_i   = -0.12310383521046273 
+alpha_z  =  1.65452092361025 
+beta_z   = -0.13315208762781655
 
-ref_lambda = {'g' : 476.60, 'r' : 640.61, 'i' : 779.49, 'z' : 917.44, 'Y' : 987.45}
 
-def create_filters(filter_file = '{}/bandpasses.fits'.format(orbdata)):
+def generate_colors(gr):
 	'''
-	Creates a dictionary of interpolated transmission functions for the DES filters 
-	Bandpasses from Burke et al 2018 AJ 155 41
+	Generates g - i and g - z colors from a nominal g - r colors, according to a linear fit 
 	'''
-	filter_table = tb.Table.read(filter_file)
-	#filter_table LAMBDA is in angstroms! Solar spectra used is in nanometers, so I'll convert first
-	filter_table['l'] = filter_table['LAMBDA']/10
-	filters = {}
-
-	
-	filters['g'] = lambda x : np.interp(x, filter_table['l'], filter_table['g'])
-	filters['r'] = lambda x : np.interp(x, filter_table['l'], filter_table['r'])
-	filters['i'] = lambda x : np.interp(x, filter_table['l'], filter_table['i'])
-	filters['z'] = lambda x : np.interp(x, filter_table['l'], filter_table['z'])
-	filters['Y'] = lambda x : np.interp(x, filter_table['l'], filter_table['Y'])
-
-	filters['LAMBDA'] = filter_table['l']
-
-	return filters
-
-des_filters = create_filters()
-
-def solar_spectrum(spec_file = '{}/solarspectrum.dat'.format(orbdata)):
-	'''
-	Interpolated solar spectrum
-	Spectrum from Meftah et al 2018 A&A 611
-	'''
-	l, f = np.loadtxt(spec_file, unpack=True)
-
-	return lambda x: np.interp(x, l, f)
-
-sun_spec = solar_spectrum()
-
-
-def reddening_colors(slope, ref_band):
-	''' 
-	Reddens the solar spectrum with a given spectral slope 
-	Passband centers from Burke et al 2018 AJ 155 41
-	'''
-
-	#l_ref = ref_lambda[ref_band]
-
-	reddened = lambda x: sun_spec(x) * np.exp(slope*x/100)
-	flux = {}
-
-	for i in ['g', 'r', 'i', 'z', 'Y']:
-		spec = lambda x : reddened(x) * des_filters[i](x)*x
-		flux[i] = simps(spec(des_filters['LAMBDA']), des_filters['LAMBDA'])
-
-	colors = {}
-	for i in ['g', 'r', 'i', 'z', 'Y']:
-		colors['{} - {}'.format(ref_band, i)] = -2.5 * np.log10(flux[ref_band]/flux[i])
-
-	return colors, flux
-
+	gi = alpha_i*gr + beta_i
+	gz = alpha_z*gr + beta_z
+	return gi , gz  
 
 class BaseLightCurve:
 	'''
@@ -78,9 +30,6 @@ class BaseLightCurve:
 		return np.zeros_like(times)
 	def __call__(self, x):
 		return self._sample(x)
-
-
-	#def __call__()
 
 class RandomPhaseLightCurve(BaseLightCurve):
 	'''
